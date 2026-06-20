5f0c4706ad614ac887b46634062860ee842dea57
Montakim <Shahriaz.Montakim@brainstation-23.com>
2026-06-20 16:19:42 +0600

    add logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
